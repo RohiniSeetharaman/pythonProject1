a64b109bddc9f20400b07d1b6d20f855883cc5c1
RohiniSeetharaman <rohininagarathna@gmail.com>
2024-11-11 21:21:14 +0530

    Nov 9 class

---
+++ November/ex_09112024_literals,variables,Datatype/Lab_009Task1_Sum,Sub,Multi,Divi.py
@@ -0,0 +1,18 @@
+#Task
+# Sum ,Sub ,Multiplication,Division with given input numbers
+number1=int(input("Enter the number1"))
+number2=int(input("Enter the number2"))
+number3=int(input("Enter the number3"))
+
+Sum=number1+number2+number3
+Sub=number1-number2-number3
+Multiplication=number1*number2*number3
+Division=number1/number2
+
+
+print("Sum of numbers is :", Sum)
+print("Sub of numbers is :", Sub)
+print("Multiplication of numbers is :", Multiplication)
+print("Division of numbers is :", Division)
+
+
---
+++ November/ex_09112024_literals,variables,Datatype/Lab_10Quotient_Remainde.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter value for number1"))
+number2=int(input("Enter value for number2"))
+Modulodivision=number1%number2
+Division=number1/number2
+
+
+print("Remainder is :",Modulodivision)
+print("Quotient is :",Division)
